f7a39476793da053c6de9c23f58ec85523ebeaa1
pujanjoci <pujanjoci2@gmail.com>
2024-06-18 12:39:10 +0545

    Map and form added

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,143 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const initialFormData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: ''
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isSubmitting || isSubmitted) {
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    fetch('https://formspree.io/f/mpzvvebg', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setIsSubmitted(true);
+      setFormData(initialFormData);
+      setIsSubmitting(false);
+      setTimeout(() => {
+        setIsSubmitted(false);
+      }, 60000);
+    })
+    .catch((error) => {
+      setIsSubmitting(false);
+    });
+  };
+
+  return (
+    <div className="max-w-lg mx-auto mt-8">
+      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8">
+        <h2 className="text-2xl font-bold mb-4 text-gray-800 text-center">Contact Us</h2>
+        <div className="mb-4 flex">
+          <div className="w-1/2 mr-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="firstName">
+              First Name
+            </label>
+            <input
+              className="shadow appearance-none border-b border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="firstName"
+              type="text"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="w-1/2 ml-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="lastName">
+              Last Name
+            </label>
+            <input
+              className="shadow appearance-none border-b border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="lastName"
+              type="text"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
+        <div className="mb-4 flex">
+          <div className="w-1/2 mr-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border-b border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="w-1/2 ml-1">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+              Phone
+            </label>
+            <input
+              className="shadow appearance-none border-b border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="phone"
+              type="text"
+              placeholder="Optional"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="shadow appearance-none border-b border-gray-200 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="message"
+            placeholder="Something to chat about."
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            rows="4"
+            required
+          ></textarea>
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${isSubmitting || isSubmitted ? 'opacity-50 cursor-not-allowed' : ''}`}
+            type="submit"
+            disabled={isSubmitting || isSubmitted}
+          >
+            {isSubmitting ? 'Submitting...' : (isSubmitted ? 'Submitted' : 'Submit')}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -8,6 +8,7 @@ const LatestNews = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
+    // Sorting posts by postId in descending order and selecting the top 3
     const sortedPosts = postsData.posts.sort((a, b) => b.postId - a.postId).slice(0, 3);
     setPosts(sortedPosts);
   }, []);
@@ -28,18 +29,18 @@ const LatestNews = () => {
 
   return (
     <div className="mx-auto max-w-6xl p-8">
-      <h2 className="text-3xl font-bold mb-4 text-center">Latest News</h2>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-3xl font-bold">Latest News</h2>
+        <a href="/news" className="text-gray-200 inline-block rounded border-2 bg-indigo-600 px-4 py-2 hover:border-blue-700 hover:bg-gray-200 hover:text-blue-700 transition duration-500 ease-in-out">Read all News</a>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {posts.map(post => (
-          <a key={post.postId} href="/news" className="bg-white p-4 shadow-md rounded-md hover:bg-gray-100 transition duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <a key={post.postId} href="/news" className="bg-white p-4 shadow-md rounded-md hover:bg-gray-100 transition duration-150 ease-in-out transform hover:-translate-y-1 hover:scale-105 block">
             <img src={images[post.postId]} alt={post.heading} className="w-full h-48 object-cover rounded-md mb-4" />
             <h3 className="text-xl font-semibold">{post.heading}</h3>
             <p className="text-md text-gray-600">{truncateDescription(post.description)}</p>
           </a>
         ))}
-      </div>
-      <div className="text-center mt-6">
-        <a href="/news" className="text-blue-600 inline-block rounded hover:text-blue-400 hover:scale-105 underline">Read all News</a>
       </div>
     </div>
   );
