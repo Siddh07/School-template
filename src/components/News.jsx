5c25944bbc0e888e0282070b51d4ac87633a9c03
Siddhant Shrestha <sidd.hant.1793@gmail.com>
2024-06-18 13:47:35 +0545

    SchoolsEvents List

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import SchoolEvents from './SchoolsEvents';
 import NewsContents from './NewsContents';
+import Layout from './Layout';
 
 const News = () => {
   return (
     <div>
       <div>
-        <h2>Latest Updates</h2>
-        <NewsContents />
+        {/* <h2>Latest Updates</h2> */}
+        {/* <NewsContents /> */}
       </div>
       <SchoolEvents />
+      {/* <Layout /> */}
+
     </div>
   );
 }
